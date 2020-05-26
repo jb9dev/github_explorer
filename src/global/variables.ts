4d030b85d62896c808c0afd90619f4349ac32576
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-26 12:10:54 -0300

    Reordenação e inclusão de uma nova

--- src/global/variables.ts
+++ src/global/variables.ts
@@ -1,10 +1,11 @@
 export const colors = {
   dark: '#3a3a3a',
   dark2: '#3d3d4d',
-  light: '#ffffff',
-  light2: '#f0f0f5',
-  green: '#04d361',
   grey: '#a8a8a8',
+  green: '#04d361',
   grey2: '#a8a8b3',
   grey3: '#cbcbd6',
+  light: '#ffffff',
+  light2: '#f0f0f5',
+  red: '#d3043d'
 };
