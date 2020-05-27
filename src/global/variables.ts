d4f52e9f8e9b8e4c75423113472d70938a7a16ee
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-27 10:13:30 -0300

    Nova variável de cor

--- src/global/variables.ts
+++ src/global/variables.ts
@@ -1,8 +1,9 @@
 export const colors = {
   dark: '#3a3a3a',
   dark2: '#3d3d4d',
-  grey: '#a8a8a8',
+  dark3: '#666666',
   green: '#04d361',
+  grey: '#a8a8a8',
   grey2: '#a8a8b3',
   grey3: '#cbcbd6',
   light: '#ffffff',
